3f4245ad9cfaba1dfb61b4d3b49bd88b3e4ac19e
Gabriel Barreto <gbrlbarreto72@hotmail.com>
2021-11-15 23:39:34 -0300

    1

--- eng_emmanuel/projeto_eng.java
+++ eng_emmanuel/projeto_eng.java
@@ -1,5 +1,19 @@
 package eng_emmanuel;
 
-public class projeto_eng {
+import java.util.Scanner;
 
+public class projeto_eng {
+    public static void main(String[] args) {
+		int a1, a2, a3;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Informe o primeiro número: ");
+		a1 = Integer.parseInt(entrada.nextLine());
+
+		System.out.println("Informe o segundo número: ");
+		a2 = Integer.parseInt(entrada.nextLine());
+
+		a3 = a1 + a2;
+		System.out.println("Valor = " + a3);
+	}
 }
